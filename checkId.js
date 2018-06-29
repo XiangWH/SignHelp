dba4c88b1c23b7ca055661ef3dea80e9a2e49c85
XiangWH <1051279132@qq.com>
2018-06-29 21:46:46 +0800

    Delete checkId.js

--- checkId.js
+++
@@ -1,7 +0,0 @@
-//在Users表中查询wxId，返回idNum和identity
-
-const { mysql } = require('../qcloud');
-module.exports = async (ctx) => {
-  var wxId = decodeURI(ctx.query.wxId);
-  ctx.state.data = await mysql('Users').select('idNum','name','identity').where('wxId',ctx.query.wxId);
-} 
